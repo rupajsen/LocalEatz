4ac986f67cd4e06f14656c2bc6ecb3581fb8e67d
Rupaj Sen <rupajps4@gmail.com>
2024-02-10 11:45:46 +0530

    push

--- MyImages/CoreData/MyReviewsContainer.swift
+++ MyImages/CoreData/MyReviewsContainer.swift
@@ -11,13 +11,24 @@ import CoreData
 class MyReviewsContainer
 {
     let container : NSPersistentContainer
+    @Published var savedEntities: [MyReviews] = []
     init()
     {
-        container = NSPersistentContainer(name: "MyReviewsDataModel")
+        container = NSPersistentContainer(name: "MyReviews")
         container.loadPersistentStores{ _, error in
             if let error {
                 print(error.localizedDescription)
             }
         }
     }
+    
+    func fetchReviews(){
+        let reviewRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "MyReviews")
+        do{
+            savedEntities = try container.viewContext.fetch(reviewRequest)
+        } catch let error{
+            print("Error fetching. \(error)")
+        }
+    }
+    
 }
